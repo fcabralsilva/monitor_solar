6f27b039077aaa40fdf05ec9a4e53cbb7f22c976
Flavio Cabral da Silva <37661589+fcabralsilva@users.noreply.github.com>
2019-10-16 21:02:06 -0300

    001 - 15/10/2019

--- monitor_solar.ino
+++ monitor_solar.ino
@@ -1,78 +1,66 @@
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <ESPAsyncWebServer.h>
-#else
-  #include <Arduino.h>
-  #include <ESP8266WiFi.h>
-  #include <Hash.h>
-  #include <ESPAsyncTCP.h>
-  #include <ESPAsyncWebServer.h>
-#endif
-#include <OneWire.h>
-#include <DallasTemperature.h>
+String VERSAO = "001 - 15/10/2019";
+//---------------------------------------
+//    INCLUINDO BIBLIOTECAS
+//---------------------------------------
+#include <WiFi.h>
+#include <DNSServer.h>
+#include <WebServer.h>
+#include <WiFiManager.h>
 
-// Data wire is connected to GPIO 4
-#define ONE_WIRE_BUS 4
-
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature sensors(&oneWire);
-
-//PINO ANALÓGICO EM QUE O SENSOR ESTÁ CONECTADO
-const int PINO_SENSOR_T = A2; 
-
-// DAS CONEXÃO WIFI
-const char* ssid = "redea";
-const char* password = "!g@t0pret0203154121";
-
-// PORTA SERVIDOR WEB
-AsyncWebServer server(80);
-
-// Replaces placeholder with DHT values
-String processor(const String& var){
-  Serial.println(var);
-  if(var == "TEMPERATUREC"){
-    return readDSTemperatureC();
-  }
-  return String();
-}
-
-void setup(){
-	//DEFINE O PINO COMO ENTRADA
-	pinMode(PINO_SENSOR_T, INPUT); 
-
-  Serial.begin(115200);
+const int SENSOR_TENSAO = 34; //PINO ANALÓGICO EM QUE O SENSOR ESTÁ CONECTADO
+const char CABECALHO_IN[] ="<!DOCTYPE HTML><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">";
+const char STYLE[] ="<style>html {font-family: Arial;display: inline-block;margin: 0px auto;text-align: center;}h2 { font-size: 3.0rem; }p { font-size: 3.0rem; }.units { font-size: 1.2rem; }.ds-labels{font-size: 1.5rem;vertical-align:middle;padding-bottom: 15px;}</style>";
+const char CABECALHO_OUT[] ="</head><body>";
+const char TITULO_H2[] ="<h2>Monitoramento Solar</h2>";
+const char TENSAO_CORRENTE[] ="<p><i class=\"fas fa-solar-panel\" style=\"color:#059e8a;\"></i> <span class=\"ds-labels\">Tensão </span> <span id=\"\">12</span><sup class=\"units\">&deg;V</sup><span class=\"ds-labels\">Corrente </span> <span id=\"\">1000</span><sup class=\"units\">&deg;mA</sup></p>";
+const char FINAL_PAGINA []="</body></html>";
+WiFiServer server(80);
+void setup() {
+  pinMode(SENSOR_TENSAO, INPUT); //DEFINE O PINO COMO ENTRADA
+  Serial.begin(115200); //INICIALIZA A SERIAL
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("AutoConnectAP");
+  Serial.println("COnectado ao WIFI :) ");
   Serial.println();
-  
-  // INICIA A LIB DO SENSOR DS18B20
-  sensors.begin();
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  Serial.println("Conectando ao WIFI ");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-	
-  Serial.println();
-  
-  // EXIBE IP OBTIDO DO ROTEADOR
-  Serial.println(WiFi.localIP());
-
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/temperaturec", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDSTemperatureC().c_str());
-  });
-	sensor_tensão(PINO_SENSOR_T);
   server.begin();
 }
- 
-void loop(){
-  
+
+void loop() {
+  WiFiManager wifiManager;
+  WiFiClient client = server.available();
+  String URL, buf;
+  if (client)
+  {
+    Serial.println("Client WEB conectado");
+    URL = client.readStringUntil('\r');
+  } else {
+    URL = "";
+  }
+  if (URL != "")
+  {
+    buf += String(CABECALHO_IN);
+    buf += String(STYLE);
+    buf += String(CABECALHO_OUT);
+    buf += String(TITULO_H2);
+    buf += String(TENSAO_CORRENTE);
+    buf += String(FINAL_PAGINA);
+    client.print(buf); client.flush(); client.stop();
+    Serial.println("Client WEB desconectado."); Serial.println(); 
+  }
+}
+// FUNÇÃO QUE REALIZA LEITURA DO SENSOR DE TENSÃO DC 25V
+float tensao_dc()
+{
+  float tensaoEntrada = 0.0; //VARIÁVEL PARA ARMAZENAR O VALOR DE TENSÃO DE ENTRADA DO SENSOR
+  float tensaoMedida = 0.0; //VARIÁVEL PARA ARMAZENAR O VALOR DA TENSÃO MEDIDA PELO SENSOR
+  float valorR1 = 30000; //VALOR DO RESISTOR 1 DO DIVISOR DE TENSÃO
+  float valorR2 = 7500; // VALOR DO RESISTOR 2 DO DIVISOR DE TENSÃO
+  int leituraSensor = 0; //VARIÁVEL PARA ARMAZENAR A LEITURA DO PINO ANALÓGICO
+  leituraSensor = analogRead(SENSOR_TENSAO); //FAZ A LEITURA DO PINO ANALÓGICO E ARMAZENA NA VARIÁVEL O VALOR LIDO
+  tensaoEntrada = (leituraSensor * 6) / 4024.0; //VARIÁVEL RECEBE O RESULTADO DO CÁLCULO
+  tensaoMedida = tensaoEntrada / (valorR2 / (valorR1 + valorR2)); //VARIÁVEL RECEBE O VALOR DE TENSÃO DC MEDIDA PELO SENSOR
+  //Serial.print("Tensão DC medida: "); //IMPRIME O TEXTO NA SERIAL
+  //Serial.print(tensaoMedida, 2); //IMPRIME NA SERIAL O VALOR DE TENSÃO DC MEDIDA E LIMITA O VALOR A 2 CASAS DECIMAIS
+  //Serial.println("V"); //IMPRIME O TEXTO NA SERIAL
+  return tensaoMedida;
 }
